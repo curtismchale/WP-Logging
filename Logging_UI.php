69bbd9c801aed53e71e890148827927a1ebf398f
curtismchale <curtis@curtismchale.ca>
2013-01-19 15:09:53 -0800

    - starting the admin UI
    Just an initial commit with the admin ui and a class

---
+++ Logging_UI.php
@@ -0,0 +1,8 @@
+<?php
+if( ! class_exists( 'WP_List_Table' ) ) {
+    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+} // class_exists( 'WP_List_Table' )
+
+class WP_Logging_UI extends WP_List_Table{
+
+} // WP_List_Table
